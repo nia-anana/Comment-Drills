3a3bdea1b90714320f848e7740c1436d39bb406e
nia-anana <77071681+nia-anana@users.noreply.github.com>
2021-05-01 12:07:10 -0500

    Add files via upload
    nevermind i got it

---
+++ commentDrills.py
@@ -0,0 +1,35 @@
+
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+#hello hi 
+END OF EXAMPLE
+
+
+'''
+START HERE
+'''
+
+#making a single line comment
+
+
+
+'''
+this is s 
+multi-line comment 
+'''
+
+
+
+var=3 #a variable single sine
+
+
+
+#i did this before
+
+
+
+''' this is another multi line comment yay 
+'''
+
+
